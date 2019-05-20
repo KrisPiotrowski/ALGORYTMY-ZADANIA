7dc56bac385d1fefdde91d519ae773a045871a4a
Krzysztof Piotrowski <krzysztof.piotrowski@microsoft.wsei.edu.pl>
2019-05-20 09:53:27 +0200

    Add project files.

---
+++ ETI06F1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ETI06F1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution();
+        }
+
+        static void Solution()
+        {
+            string[] tab = (Console.ReadLine()).Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            double[] tabFloat = Array.ConvertAll(tab, double.Parse);
+            double r = tabFloat[0];
+            double d = tabFloat[1];
+
+            if (d >= 2 * r || d < 1 || 2 * r > 2000)
+                throw new ArgumentException();
+
+            double newR2 = r * r - Math.Pow((d / 2), 2);
+
+            double result = Math.Round(3.141592654 * newR2,2);
+
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
---
+++ EUCGAME/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+
+namespace EUCGAME
+{
+    class Program
+    {
+        static StringBuilder wynik = new StringBuilder(100000);
+
+        static void Main(string[] args)
+        {
+
+            int Q = int.Parse(Console.ReadLine());
+
+            if (Q <= 0 || Q >= 11)
+                throw new ArgumentException();
+
+            for (int i = 1; i <= Q; i++)
+            {
+                bool num = false;
+                if (i == Q)
+                    num = true;
+                Solution(num);
+            }
+            Console.WriteLine(wynik.ToString());
+        }
+
+        static void Solution(bool j)
+        {
+
+            string[] tab = (Console.ReadLine()).Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            int[] tabInt = Array.ConvertAll(tab, int.Parse);
+            int a = tabInt[0];
+            int b = tabInt[1];
+            int result = 0;
+
+            if (a <= 0 || b <= 0 || a>= 1000000001 || b >= 1000000001)
+                throw new ArgumentException();
+
+            while ((a > b  && a - b > 0) || (b > a && b - a > 0))
+            {
+                if (a > b)
+                    a -= b;
+                else if (b > a)
+                    b -= a;
+            }
+
+            result = a + b;
+
+            if (!j)
+                wynik.Append($"{result}\n");
+            else
+                wynik.Append($"{result}");
+        }
+    }
+}
