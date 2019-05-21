feadf6c4c724a91b98cc3a0bade1134d6cb31865
Krzysztof Piotrowski <krzysztof.piotrowski@microsoft.wsei.edu.pl>
2019-05-21 18:38:20 +0200

    FR_10_01

--- FR_09_01/Program.cs
+++ FR_09_01/Program.cs
@@ -9,22 +9,13 @@ namespace FR_09_01
             string[] tab = (Console.ReadLine()).Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
             int[] tabInt= Array.ConvertAll(tab, int.Parse);
 
-            //Console.WriteLine("sprawdzanie minus i plus - sprawdzenie");
-            //Console.WriteLine("sprawdz minus(4, -1 ): " + SprawdzMinus(4, -1));
-            //Console.WriteLine("sprawdz minus(4, -5 ): " + SprawdzMinus(4, -5));
-            //Console.WriteLine("sprawdz minus(4, -4): " + SprawdzMinus(4, -4));
-            //Console.WriteLine("sprawdz plus(4, 1 ): " + SprawdzPlus(4, 1));
-            //Console.WriteLine("sprawdz plus(4, 5 ): " + SprawdzPlus(4, 5));
-            //Console.WriteLine("sprawdz plus(4, 4): " + SprawdzPlus(4, 4));
-
             if (tabInt[0] == 0 && tabInt[1] == 0)
                 Console.Write(4);
-            else if ((double)tabInt[2] == Math.Sqrt(((double)tabInt[0]* (double)tabInt[0])+ (double)(tabInt[1]* (double)tabInt[1])))
+            else if (tabInt[2] == Math.Sqrt((tabInt[0]* tabInt[0])+ (tabInt[1]* tabInt[1])))
                 Console.Write(3);
             else
             {
-                int x = Sprawdz(tabInt[2], tabInt[1]) + Sprawdz(tabInt[2], tabInt[0]);
-                Console.Write(x);
+                Console.Write(Sprawdz(tabInt[2], tabInt[1]) + Sprawdz(tabInt[2], tabInt[0]));
             }
 
         }
---
+++ FR_10_01/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace FR_10_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution();
+        }
+
+        static void Solution()
+        {
+
+            string[] tab = (Console.ReadLine()).Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            int[] tabInt = Array.ConvertAll(tab, int.Parse);
+
+            int cntX = 1;
+
+            if (tabInt[2] != tabInt[0])
+                cntX += 1;
+
+            if (tabInt[4] != tabInt[2] && tabInt[4] != tabInt[0])
+                cntX += 1;
+
+            if (tabInt[6] != tabInt[4] && tabInt[6] != tabInt[2] && tabInt[6] != tabInt[0])
+                cntX += 1;
+
+            int cntY = 1;
+
+            if (tabInt[3] != tabInt[1])
+                cntY += 1;
+
+            if (tabInt[5] != tabInt[3] && tabInt[5] != tabInt[1])
+                cntY += 1;
+
+            if (tabInt[7] != tabInt[5] && tabInt[7] != tabInt[3] && tabInt[7] != tabInt[1])
+                cntY += 1;
+
+            if (cntX == 2 && cntY == 2)
+                Console.WriteLine("Tak");
+            else Console.WriteLine("Nie");
+        }
+    }
+}
