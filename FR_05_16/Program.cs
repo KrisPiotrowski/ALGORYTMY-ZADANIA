91f777f95d2452907f3793b7aadbfabc874f0a04
Krzysztof Piotrowski <krzysztof.piotrowski@microsoft.wsei.edu.pl>
2019-05-29 20:57:53 +0200

    mrowki

---
+++ FR_05_16/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FR_05_16
+{
+    class Program
+    {
+        static Stack<int> s = new Stack<int>();
+        static void Main(string[] args)
+        {
+         
+                int Q = int.Parse(Console.ReadLine());
+
+                for (int i = 1; i <= Q; i++)
+                {
+                    string[] tab = (Console.ReadLine()).Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    if (tab[0] == "1")
+                    {
+                        s.Push(Convert.ToInt32(tab[1]));
+                    }
+                    else
+                    {
+                        int x = s.Pop();
+                        int y = s.Pop();
+                        if (tab[1] == "*")
+                            s.Push(y * x);
+                        else if (tab[1] == "+")
+                            s.Push(y + x);
+                        else if (tab[1] == "/")
+                            s.Push(y / x);
+                        else if (tab[1] == "-")
+                            s.Push(y - x);
+                    //Console.WriteLine("szczyt stosu: "+s.Peek());
+                    }
+                }
+                Console.WriteLine(s.Pop());
+                Console.WriteLine("ONP");
+      
+        }
+    }
+}
---
+++ FR_09_12/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FR_09_12
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            int Q = int.Parse(Console.ReadLine());
+
+            int[,] tab = new int[2,Q];
+
+            for (int i = 0; i < Q; i++)
+            {
+                int[] x = GetNumber1();
+                tab[0, i] = x[0];
+                tab[1, i] = x[1];
+            }
+
+            Stack<int> s = new Stack<int>();
+            s.Push(tab[1,0]);
+
+            for (int i = 1; i < Q; i++)
+            {
+                if(tab[1,i] > s.Peek())
+                {
+                    s.Push(tab[1,i]);
+                }
+                else if (tab[1,i] == s.Peek() && (tab[0,i] - tab[0,i-1] > 1))
+                {
+                    s.Push(tab[1, i]);
+                }
+                else if (tab[1,i] < s.Peek())
+                {
+                    while (s.Count > 0 && s.Peek() > tab[1,i])
+                    {
+                        s.Pop();
+                    }
+                    s.Push(tab[1,i]);
+                }
+                //Console.WriteLine("i: " + i + ", s.Cnt: " + s.Count);
+            }
+
+            Console.WriteLine(s.Count);
+        }
+
+        static int[] GetNumber1()
+        {
+            string[] tab = (Console.ReadLine()).Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            return Array.ConvertAll(tab,int.Parse);
+        }
+    }
+}
